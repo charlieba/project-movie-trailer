73e7248af89c2a9bfc7e78eba1c4f4769a1b020a
giovanib02 <giovanib07@gmail.com>
2016-11-25 13:12:13 -0600

    PEP-8

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 import webbrowser
+
+
 class Movie():
-    
     """ This Class provides a way to store movie releated information
 
         Attributes:
@@ -10,19 +11,21 @@ class Movie():
         trailer   (str): Youtube link of the trailer of the movie
 
         Methods:
-        show_trailer (self); Open the web browser showing and playing the movie trailer
+        show_trailer (self); Open the web browser showing and playing 
+        the movie trailer
 
     """
-    
-    # This is the constructor method for initialization the properties of a movie
-    def __init__(self, movie_title,movie_storyline,poster_image,
+    def __init__(self, movie_title, movie_storyline, poster_image, 
                  trailer_youtube):  
-        self.title=movie_title  # Title, save the title of the movie
-        self.storyline=movie_storyline  # Story Line, save the description of the movie
-        self.poster_image_url=poster_image  # Poster, save the highlight image of the movie
-        self.trailer_youtube_url=trailer_youtube # Trailer, save the youtube link with the trailer
+        # Title, save the title of the movie
+        self.title = movie_title  
+        # Story Line, save the description of the movie
+        self.storyline = movie_storyline  
+        # Poster, save the highlight image of the movie
+        self.poster_image_url = poster_image  
+        # Trailer, save the youtube link with the trailer
+        self.trailer_youtube_url = trailer_youtube 
 
     # This method open the web browser showing and playing the movie trailer    
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
